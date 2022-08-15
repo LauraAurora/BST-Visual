1602a65bb5606a576f6b6026c91ec715549def78
[LauraAurora] <[laura.apenza@gmail.com]>
2022-08-14 20:31:23 -0400

    Initial commit

---
+++ src/TreeVisualizer/TreeVisualizer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+export class TreeVisualizer extends React.Component {
+
+    constructor()
+    {
+        super();
+    }
+    
+    /*var destination = document.querySelector("#tree-container");
+
+    ReactDOM.render(
+    <div>
+        {renderData}
+    </div>,
+    destination
+    );
+    */
+}
